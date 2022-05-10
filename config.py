6f6bc8cb1e5228be3af84c26f6f849f09e2df2f4
FeverCode <giddylancs.gl@gmail.com>
2022-05-10 06:18:02 +0300

    track modification

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
